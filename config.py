06e116e15b5bbfa191cc63b48227fbadf413ecf7
impanteleeva <impanteleyeva@gmail.com>
2019-06-07 00:18:39 +0300

    encoding

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ PATH_UDPIPE_MODEL = os.path.join(BASE_DIR, 'data', 'models', 'english-partut-ud-
 UDPIPE_MODEL = Model(PATH_UDPIPE_MODEL)
 
 PATH_LISTS = os.path.join(BASE_DIR, 'data', 'lists', 'lists.json')
-with open(PATH_LISTS) as data_file:
+with open(PATH_LISTS, encoding='utf-8') as data_file:
     lists = json.load(data_file)
 FIVE_T_FREQ_COCA = lists['5000frequentCOCA']
 FREQ_VERBS_COCA_FROM_FIVE_T = lists['frequentverbsCOCAfrom5000']
@@ -21,15 +21,15 @@ UWL = lists['UWL']
 OPEN_CLASS = ['NOUN', 'VERB', 'ADV', 'ADJ', 'PROPN']
 
 PATH_LINKINGS = os.path.join(BASE_DIR, 'data', 'lists', 'linkings.json')
-with open(PATH_LINKINGS) as data_file:
+with open(PATH_LINKINGS, encoding='utf-8') as data_file:
     LINKINGS = json.load(data_file)
 
 PATH_FUNC_NGRAMS = os.path.join(BASE_DIR, 'data', 'lists', 'functional_ngrams.json')
-with open(PATH_FUNC_NGRAMS) as data_file:
+with open(PATH_FUNC_NGRAMS, encoding='utf-8') as data_file:
     FUNC_NGRAMS = json.load(data_file)
 
 PATH_SUFFIXES = os.path.join(BASE_DIR, 'data', 'lists', 'suffixes.json')
-with open(PATH_SUFFIXES) as data_file:
+with open(PATH_SUFFIXES, encoding='utf-8') as data_file:
     SUFFIXES = json.load(data_file)
 
 PATH_NGRAMS = os.path.join(BASE_DIR, 'data', 'lists', 'ngrams.txt')
